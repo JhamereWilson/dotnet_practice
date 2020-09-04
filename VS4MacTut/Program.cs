0b95be57ea9dc8820bd9259e29de3f91076ac663
jhamerewilson <jhamerewilson@outlook.com>
2020-09-04 14:29:52 -0400

    First Commit

---
+++ VS4MacTut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VS4MacTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
